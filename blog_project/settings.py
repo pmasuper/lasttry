dec46e32cc55ad413af49d14eba2ecfe02887f49
Prince <pmasuper34@gmail.com>
2021-04-30 20:26:02 +0500

    Added emails

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -124,3 +124,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_HOST_USER = 'pmasuper34@gmail.com'
+EMAIL_HOST_PASSWORD = 'persiantiger'
+EMAIL_USE_TLS = True
