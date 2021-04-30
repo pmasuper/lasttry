368fc60045d46d83374f5d806516b08d06e3dc62
Prince <pmasuper34@gmail.com>
2021-04-30 21:01:44 +0500

    added tags

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-# from taggit.managers import TaggableManager
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -22,7 +22,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
     objects = models.Manager()
     published = PublishedManager()
-    # tags = TaggableManager()
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
